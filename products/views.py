b36d5bf12aa41084f85f4b0bbeceec25ebf676c6
Alex_K <79103208963@ya.ru>
2021-06-02 00:50:42 +0300

    add url

--- products/views.py
+++ products/views.py
@@ -8,21 +8,21 @@ def index(request):
 def products(request):
     return render(request, 'products/products.html')
 
-def test_context(request):
-    with open('products/fixtures/goods_my.json', 'r', encoding='utf-8') as f_obj:
-        data = json.load(f_obj)
-
-    print(data)
-    print(type(data))
-    context = {
-        'title': 'G-Shop',
-        'header': 'Добро пожаловать',
-        'username': 'Сидор Сидоров',
-        'products': data,
-        #     [
-        #     {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
-        #     {'name': 'Синяя куртка The North Face', 'price': 23725},
-        #     {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
-        # ]
-    }
-    return render(request, 'products/test-context.html', context)
+# def test_context(request):
+#     with open('products/fixtures/goods_my.json', 'r', encoding='utf-8') as f_obj:
+#         data = json.load(f_obj)
+#
+#     print(data)
+#     print(type(data))
+#     context = {
+#         'title': 'G-Shop',
+#         'header': 'Добро пожаловать',
+#         'username': 'Сидор Сидоров',
+#         'products': data,
+#         #     [
+#         #     {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
+#         #     {'name': 'Синяя куртка The North Face', 'price': 23725},
+#         #     {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
+#         # ]
+#     }
+#     return render(request, 'products/test-context.html', context)
