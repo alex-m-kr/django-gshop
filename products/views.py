40db2d6bebe61775d103fb902da6baac1567aaf0
Alex_K <79103208963@ya.ru>
2021-06-02 03:15:15 +0300

    add lesson2

--- products/views.py
+++ products/views.py
@@ -3,10 +3,19 @@ import json
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop',
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    with open('products/fixtures/goods_my.json', 'r', encoding='utf-8') as f_obj:
+        data_from_json = json.load(f_obj)
+    context = {
+        'title': 'GeekShop - Каталог',
+        'products': data_from_json,
+    }
+    return render(request, 'products/products.html', context)
 
 # def test_context(request):
 #     with open('products/fixtures/goods_my.json', 'r', encoding='utf-8') as f_obj:
