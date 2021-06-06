0c5a31be665d70990b63126c6c212b54df138462
Alex_K <79103208963@ya.ru>
2021-06-07 00:58:33 +0300

    add ProductCategory, Product

--- products/views.py
+++ products/views.py
@@ -16,22 +16,3 @@ def products(request):
         'products': data_from_json,
     }
     return render(request, 'products/products.html', context)
-
-# def test_context(request):
-#     with open('products/fixtures/goods_my.json', 'r', encoding='utf-8') as f_obj:
-#         data = json.load(f_obj)
-#
-#     print(data)
-#     print(type(data))
-#     context = {
-#         'title': 'G-Shop',
-#         'header': 'Добро пожаловать',
-#         'username': 'Сидор Сидоров',
-#         'products': data,
-#         #     [
-#         #     {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
-#         #     {'name': 'Синяя куртка The North Face', 'price': 23725},
-#         #     {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
-#         # ]
-#     }
-#     return render(request, 'products/test-context.html', context)
