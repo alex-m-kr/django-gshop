94c3b340dfa209f559447e81b1a06906e4547ed3
Alex_K <79103208963@ya.ru>
2021-06-12 15:01:08 +0300

    added profile

--- users/views.py
+++ users/views.py
@@ -34,6 +34,9 @@ def register(request):
     context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'users/register.html', context)
 
+def profile(request):
+    return render(request, 'users/profile.html')
+
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
