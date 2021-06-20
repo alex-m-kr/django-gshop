895d71d60569933162530e83637725d441780c67
Alex_K <79103208963@ya.ru>
2021-06-21 01:32:02 +0300

    add Update

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from users.models import User
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 
 # Create your views here.
@@ -10,16 +12,39 @@ def index(request):
 
 # Здесь будет CRUD
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {'users': User.objects.all(),
+               'title': 'GeekShop - АДМИН | Пользователи'}
     return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegisterForm()
+    context = {'title': 'GeekShop - АДМИН | Регистрация', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
 
 
 def admin_users_update(request, id):
-    return render(request, 'admins/admin-users-update-delete.html')
+    selected_user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES,
+                                    instance=selected_user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'GeekShop - АДМИН | обновление пользователя',
+        'form': form,
+        'selected_user': selected_user,
+    }
+    return render(request, 'admins/admin-users-update-delete.html', context)
 
 
 def admin_users_delete(request, id):
